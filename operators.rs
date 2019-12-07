4c67c27fe120eabbe13941ecb41c9e8589153624
Musab Kılıç <musabkilic@protonmail.com>
2019-12-07 20:18:44 +0300

    add new features

--- operators.rs
+++ operators.rs
@@ -3,18 +3,21 @@ extern crate eval;
 use eval::{eval};
 
 fn main(){
-    for i in ["+", "-", ""].iter(){
-        for j in ["+", "-", ""].iter(){
-            for k in ["+", "-", ""].iter(){
-                for l in ["+", "-", ""].iter(){
-                    for m in ["+", "-", ""].iter(){
-                        for n in ["+", "-", ""].iter(){
-                            for o in ["+", "-", ""].iter(){
-                                for p in ["+", "-", ""].iter(){
+    let mut num = 0;
+    let operator_list = [" + ", " - ", ""];
+    for i in operator_list.iter(){
+        for j in operator_list.iter(){
+            for k in operator_list.iter(){
+                for l in operator_list.iter(){
+                    for m in operator_list.iter(){
+                        for n in operator_list.iter(){
+                            for o in operator_list.iter(){
+                                for p in operator_list.iter(){
                                     let exp = format!("1{}2{}3{}4{}5{}6{}7{}8{}9", i, j, k, l, m, n, o, p);
                                     let result = eval(&exp).unwrap();
                                     if result == 100{
-                                        println!("{}=100", exp);
+                                        println!("{} = 100", exp);
+                                        num += 1;
                                     }
                                 }
                             }
@@ -24,4 +27,5 @@ fn main(){
             }
         }
     }
+    println!("\nFound {} solutions!!", num);
 }
